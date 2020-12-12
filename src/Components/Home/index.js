b7e17379a51866b35687d1cc48e60390b0a3d291
juliavalerio <juliav286@gmail.com>
2020-12-12 19:43:05 -0300

    new feature: starting the basic structure of the project

---
+++ src/Components/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css'
+
+const Home = () => {
+    return(
+        <div className='container-home'>
+            <div>
+                <h1>Game Quiz</h1>
+                <p>Seja bem-vindo(a)! A partir de agora, você terá a oportunidade de colocar em prática tudo o que já aprendeu. Para isso, será necessário superar alguns desafios, tomando a <b>melhor decisão para você, para o cliente e para o negócio</b>. Está preparado?</p>
+                <button>Clique aqui para aceitar o desafio!</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
