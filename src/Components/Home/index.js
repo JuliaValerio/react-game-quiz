26ed027b1aa8664368d8968fe058ad6cfd7d1677
juliavalerio <juliav286@gmail.com>
2020-12-12 20:32:24 -0300

    home component created and stylized, with responsive.

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -4,10 +4,10 @@ import './style.css'
 const Home = () => {
     return(
         <div className='container-home'>
-            <div>
-                <h1>Game Quiz</h1>
-                <p>Seja bem-vindo(a)! A partir de agora, você terá a oportunidade de colocar em prática tudo o que já aprendeu. Para isso, será necessário superar alguns desafios, tomando a <b>melhor decisão para você, para o cliente e para o negócio</b>. Está preparado?</p>
-                <button>Clique aqui para aceitar o desafio!</button>
+            <div className='content-home'>
+                <h1 className="title">Game Quiz</h1>
+                <p>Seja bem-vindo(a)! A partir de agora, você terá a oportunidade de colocar em prática tudo o que já aprendeu. Para isso, será necessário superar alguns desafios, tomando a <b>melhor decisão para você, <br/> para o cliente e para <br/> o negócio</b>. Está preparado?</p>
+                <button className="btn-challenge">Clique aqui para aceitar o desafio!</button>
             </div>
 
         </div>
